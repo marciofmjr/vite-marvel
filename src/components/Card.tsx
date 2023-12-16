e9f66b6a4c2331a1c763cb17c00137ff57d883ac
Marcio Junior <marciofmjr@hotmail.com>
2023-12-15 22:31:15 -0300

    set responsive cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,9 @@
 export default function Card({ name, image }: { name: string; image: string}) {
 	return (
 		<>
-			<div className="w-64 border-solid border-slate-300 border-2 rounded-lg overflow-hidden shadow-md">
+			<div className="bg-white border-solid border-slate-300 border-2 rounded-lg overflow-hidden shadow-md">
 				<img src={image} alt="Hero" className="w-full aspect-square object-cover object-center" />
-				<div className="bg-white p-4">
+				<div className="p-4">
 					<h2 className="text-xl font-medium">{ name }</h2>
 				</div>
 			</div>
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,7 +4,8 @@ import characters from './../data/characters.json'
 export default function Cards() {
 	return (
 		<>
-			<div className="flex justify-between flex-wrap gap-12">
+			{/* <div className="flex justify-between flex-wrap md:gap-12"> */}
+			<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:gap-6 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
 				{ characters.map(char => (
 					<Card key={char.id} name={char.name} image={char.image} />
 				)) }
